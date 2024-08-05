d5c160abecf4915e8315b35dec92a10a23c52ad9
swapnil <swapnil.zakade.33@gmail.com>
2024-08-05 19:52:10 +0530

    add some data in constants.

--- src/config/constants/constant.js
+++ src/config/constants/constant.js
@@ -36,11 +36,21 @@ export const CURRENCIES = [
 // Example Job Titles/REQs (this should be dynamically loaded based on the selected client)
 export const JOB_TITLES = [
   { id: "", title: "Select Job Title", reqID: "", clientId: "" },
-  { id: "REQ001", title: "Software Developer", reqID: "REQ001", clientId: "1" },
+  {
+    id: "REQ001",
+    title: "Application Development",
+    reqID: "REQ001",
+    clientId: "1",
+  },
   { id: "REQ002", title: "Data Scientist", reqID: "REQ002", clientId: "1" },
-  { id: "REQ003", title: "Product Manager", reqID: "REQ003", clientId: "1" },
-  { id: "REQ004", title: "Data Scientist", reqID: "REQ004", clientId: "2" },
-  { id: "REQ005", title: "Product Manager", reqID: "REQ005", clientId: "2" },
+  { id: "REQ003", title: "Product Management", reqID: "REQ003", clientId: "1" },
+  {
+    id: "REQ004",
+    title: "Software Development",
+    reqID: "REQ004",
+    clientId: "2",
+  },
+  { id: "REQ005", title: "Web Development", reqID: "REQ005", clientId: "2" },
 ];
 
 // Example Talents (this should be dynamically loaded based on the selected job title)
@@ -50,7 +60,12 @@ export const TALENTS_USERS = [
   { id: "3", name: "Riddhesh", reqID: "REQ001" },
   { id: "4", name: "Abhilash", reqID: "REQ002" },
   { id: "5", name: "Bhavesh", reqID: "REQ002" },
-  { id: "6", name: "Mahesh", reqID: "REQ003" },
-  { id: "7", name: "Siddhsh", reqID: "REQ003" },
-  { id: "8", name: "Chetan", reqID: "REQ003" },
+  { id: "6", name: "Amit", reqID: "REQ002" },
+  { id: "7", name: "Mahesh", reqID: "REQ003" },
+  { id: "8", name: "Siddhesh", reqID: "REQ003" },
+  { id: "9", name: "Chetan", reqID: "REQ003" },
+  { id: "9", name: "Prajwal", reqID: "REQ004" },
+  { id: "9", name: "Sapna", reqID: "REQ004" },
+  { id: "9", name: "Krishna", reqID: "REQ005" },
+  { id: "9", name: "Sangeeta", reqID: "REQ005" },
 ];
